86017f54dd385c48b2b55d94c673714d8111638c
HerculesHLMN <hercules.nahirnei2018@gmail.com>
2026-08-19 18:55:11 -0300

    Adiciona EX03

--- ExerciciosDeMetodos/src/metodoParametro_EX03.java
+++ ExerciciosDeMetodos/src/metodoParametro_EX03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class metodoParametro1_EX03 {
+public class metodoParametro_EX03 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
