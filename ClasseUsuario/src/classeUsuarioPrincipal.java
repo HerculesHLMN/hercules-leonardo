c572050b03ca271812807befd996330fa250553d
hercules_leonardo <hercules.nahirnei2018@gmail.com>
2026-09-01 19:11:22 -0300

    Adiciona EX Classe Usuario

--- ClasseUsuario/src/classeUsuarioPrincipal.java
+++ ClasseUsuario/src/classeUsuarioPrincipal.java
@@ -10,6 +10,15 @@ public class classeUsuarioPrincipal {
           u1.dataNascimento = "25/01/89";
           u1.estadoCivil = "Casada";
 
-          System.out.println("\n" + "Nome: " + u1.nome + "\n" + "Sexo: " + u1.sexo + "\n" + "");
+          u2.nome = "Lucas";
+          u2.sexo = 'M';
+          u2.dataNascimento = "11/02/2010";
+          u2.estadoCivil = "Solteiro";
+
+          System.out.println("\n" + "Nome: " + u1.nome + "\n" + "Sexo: " + u1.sexo);
+          System.out.println("Data Nascimento: " + u1.dataNascimento + "\n" + "Estado Civíl: " + u1.estadoCivil);
+          System.out.println("\n" + "Nome: " + u2.nome + "\n" + "Sexo: " + u2.sexo);
+          System.out.println("Data Nascimento: " + u2.dataNascimento + "\n" + "Estado Civíl: " + u2.estadoCivil);
+
      }
 }
